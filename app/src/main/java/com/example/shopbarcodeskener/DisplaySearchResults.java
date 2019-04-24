2fdfd41419aa7a36d12d856af197e16ccc7bb282
Nenad Anđelković <badnenada@googlemail.com>
2019-04-24 02:26:15 +0200

    added Volley for REST, and made simple test views as to make sure it works.

--- app/src/main/java/com/example/shopbarcodeskener/DisplaySearchResults.java
+++ app/src/main/java/com/example/shopbarcodeskener/DisplaySearchResults.java
@@ -4,9 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class DisplaySearchResults extends AppCompatActivity {
+    public String searchResults = "ccc";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +29,55 @@ public class DisplaySearchResults extends AppCompatActivity {
 
         //Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
-        String searchResults = intent.getStringExtra(MainActivity.SEARCH_MESSAGE);
+        String search = intent.getStringExtra(MainActivity.SEARCH_MESSAGE);
+        final TextView textView = findViewById(R.id.textViewSearchResult);
+
+        TextView testView = findViewById(R.id.textViewTest);
+        testView.setText(search);
+
+        JSONArray jsonObject;
+
+
+
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, search,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        searchResults = response;
+                        try {
+                            JSONObject jsonObjectTemp = new JSONObject(searchResults);
+                            JSONArray jsonArray = jsonObjectTemp.getJSONArray("records");
+                            for (int i = 0; i < 1; i++) {
+                                JSONObject explrObject = jsonArray.getJSONObject(i);
+                                textView.setText(explrObject.toString());
+                            }
+                        } catch (JSONException e) {
+                            //TODO
+                        }
+
+
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                searchResults = "Nema ili nije nasao";
+                textView.setText(searchResults);
+            }
+        });
+
+        requestQueue.add(stringRequest);
 
         //Capture the layout of TextView and set the string as its text
 
-        TextView textView = findViewById(R.id.textViewSearchResult);
+
         textView.setText(searchResults);
+        textView.setMovementMethod(new ScrollingMovementMethod());
 
     }
 }
--- app/src/main/java/com/example/shopbarcodeskener/MainActivity.java
+++ app/src/main/java/com/example/shopbarcodeskener/MainActivity.java
@@ -7,22 +7,48 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String SEARCH_MESSAGE = "com.example.shopbarcodeskener.SEARCH";
+
+    public String baseUrl = "https://srecniljudi.com/api/product/search.php?s=";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+
 
     public void searchMessage(View view){
 
         Intent intent = new Intent(this, DisplaySearchResults.class);
         EditText editText = findViewById(R.id.editTextSearchInput);
         String message = editText.getText().toString();
-        intent.putExtra(SEARCH_MESSAGE, message);
+        String search = baseUrl + message;
+
+
+
+
+
+        intent.putExtra(SEARCH_MESSAGE, search);
         startActivity(intent);
 
 
