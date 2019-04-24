1e43f52ca76cb51f54f3b5452fbcafa6423727e9
Nenad Anđelković <badnenada@googlemail.com>
2019-04-24 12:58:28 +0200

    made changes to the view layout

--- app/src/main/java/com/example/shopbarcodeskener/DisplaySearchResults.java
+++ app/src/main/java/com/example/shopbarcodeskener/DisplaySearchResults.java
@@ -1,26 +1,33 @@
 package com.example.shopbarcodeskener;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.URI;
+import java.net.URL;
+
 public class DisplaySearchResults extends AppCompatActivity {
     public String searchResults = "ccc";
+    //public String defaultUrl = "https://www.srecniljudi.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +39,7 @@ public class DisplaySearchResults extends AppCompatActivity {
         String search = intent.getStringExtra(MainActivity.SEARCH_MESSAGE);
         final TextView textView = findViewById(R.id.textViewSearchResult);
 
-        TextView testView = findViewById(R.id.textViewTest);
+        final TextView testView = findViewById(R.id.textViewTest);
         testView.setText(search);
 
         final TextView textViewProduct = findViewById(R.id.textViewProductName);
@@ -41,10 +48,7 @@ public class DisplaySearchResults extends AppCompatActivity {
 
         final TextView textViewPrice = findViewById(R.id.TextViewPriceField);
 
-        JSONArray jsonObject;
-
-
-
+        final ImageView imageView = findViewById(R.id.imageView);
 
 
 
@@ -54,26 +58,34 @@ public class DisplaySearchResults extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
+
                         searchResults = response;
                         Double price = 0.0;
+
                         try {
                             JSONObject jsonObjectTemp = new JSONObject(searchResults);
                             JSONArray jsonArray = jsonObjectTemp.getJSONArray("records");
                             for (int i = 0; i < 1; i++) {
                                 JSONObject explrObject = jsonArray.getJSONObject(i);
 
-                                textView.setText(explrObject.toString());
+                                textView.setVisibility(View.GONE);
+                                testView.setVisibility(View.GONE);
                                 textViewProduct.setText(explrObject.getString("name"));
                                 textViewSku.setText(explrObject.getString("sku"));
-                                price = Math.ceil(explrObject.getDouble("price") * 1.2); //with tax
+                                price = Math.ceil(explrObject.getDouble("price") * 1.2/10)*10; //with tax
                                 textViewPrice.setText(price.toString());
+                                String url = "https://www.srecniljudi.com/" + explrObject.getString("url_thumb");
+
+
+                                Picasso.get()
+                                        .load(url)
+                                        .placeholder(R.drawable.zec)
+                                        .into(imageView);
+
                             }
                         } catch (JSONException e) {
                             //TODO
                         }
-
-
 
 
                     }
@@ -88,7 +100,6 @@ public class DisplaySearchResults extends AppCompatActivity {
         requestQueue.add(stringRequest);
 
         //Capture the layout of TextView and set the string as its text
-
 
         textView.setText(searchResults);
         textView.setMovementMethod(new ScrollingMovementMethod());
